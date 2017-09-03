43bcb094b533076f4cdc1a73feb68760293240c1
carloscacho <carlosemilio.ac@gmail.com>
2017-09-03 04:27:25 -0400

    commit do sono
    fuiiii

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # innoveAgro
+
+
+## Requerimentos
+python 2.7
+openCV
+numPY
+
+
+# Run
+python teste.py <R1> <G1> <B1> <R2> <G2> <B2>
+
+um intervalo de uma cor exemplo amarelo
+ex:
--- teste.py
+++ teste.py
@@ -30,14 +30,22 @@ blue = float(sys.argv[1])/255.0
 green = float(sys.argv[2])/255.0
 red = float(sys.argv[3])/255.0
 
+blue2 = float(sys.argv[4])/255.0
+green2 = float(sys.argv[5])/255.0
+red2 = float(sys.argv[6])/255.0
+
 color = np.float32([[[blue, green, red]]])
 hsv_color = cv2.cvtColor(color, cv2.COLOR_BGR2HSV)
 
+color2 = np.float32([[[blue2, green2, red2]]])
+hsv_color2 = cv2.cvtColor(color2, cv2.COLOR_BGR2HSV)
+
 hue = hsv_color[0][0][0]
+hue2 = hsv_color2[0][0][0]
 hsf_select = 5;
 
 
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0)
 
 
 seletor = Regular(1)
@@ -54,7 +62,9 @@ while(1):
     # Take each frame
 
     # _, frame = cap.read()
-    frame  = cv2.imread('soja4.jpeg')
+    frame  = cv2.imread('soja6.jpeg')
+
+
 
     if jogafora:
         jogafora = False
@@ -73,9 +83,17 @@ while(1):
         print hsv[0][0][0]
 
         # define range of blue color in HSV
-        lower_blue = np.array([hue-hsf_select,0,0])
-        upper_blue = np.array([hue+hsf_select,1,1])
+        lower_blue = np.array([hue-2,0,0])
+        upper_blue = np.array([hue2+2,1,1])
 
+        #smothing
+        kernel = np.ones((5,5),np.float32)/25
+        hsv = cv2.filter2D(hsv,-1,kernel)
+        hsv = cv2.blur(hsv,(5,5))
+
+        foto = hsv.astype(unit8)
+
+        circles = cv2.HoughCircles(foto,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
         # Threshold the HSV image to get only blue colors
         mask = cv2.inRange(hsv, lower_blue, upper_blue)
 

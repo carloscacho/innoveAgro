fc9a9e6dc371042145475488f3c00951c142a450
carloscacho <carlosemilio.ac@gmail.com>
2017-09-04 11:11:25 -0400

    Refatoração
    Realizando analiser mais precisas

--- teste.py
+++ teste.py
@@ -2,15 +2,10 @@ import cv2
 import numpy as np
 from pylab import *
 
-hsf_select = 0;
-min_value = 100;
-max_value = 255;
-select = True;
-troquei = False;
-
-
-# verde
+contabil = []
+#definicoo do intervalo de cores
 cores = []
+# verde
 cores.append([33,187,200])
 cores.append([41,176,184])
 #amarelo
@@ -19,21 +14,11 @@ cores.append([55,174,189])
 #marron-escuro
 cores.append([47,87,129])
 cores.append([81,181,216])
-
-contabil = []
+#passar pela imagem uma vez para cada cor
 for cor in range(0,3):
     print "cor " + str(cor)
 
-
-    # blue = float(sys.argv[1])/255.0
-    # green = float(sys.argv[2])/255.0
-    # red = float(sys.argv[3])/255.0
-    #
-    # blue2 = float(sys.argv[4])/255.0
-    # green2 = float(sys.argv[5])/255.0
-    # red2 = float(sys.argv[6])/255.0
-
-
+    #carrega uma cor para ser analisada
     blue = cores[cor*2][0]
     green = cores[cor*2][1]
     red = cores[cor*2][2]
@@ -42,6 +27,7 @@ for cor in range(0,3):
     green2 = cores[cor*2+1][1]
     red2 = cores[cor*2+1][2]
 
+    #transforma a cor que sera analisada no momento de  RGB em HSF
     color = np.float32([[[blue, green, red]]])
     hsv_color = cv2.cvtColor(color, cv2.COLOR_BGR2HSV)
 
@@ -51,59 +37,58 @@ for cor in range(0,3):
     hue = hsv_color[0][0][0]
     hue2 = hsv_color2[0][0][0]
 
+    #capitura de vido
     #cap = cv2.VideoCapture(0)
 
     # while(1):
 
-    # Take each frame
-
+    # Coleta dos frames
     # _, frame = cap.read()
-    frame  = cv2.imread('soja6.jpeg')
+    frame  = cv2.imread('amosta.jpeg')
 
+    #jogar fora metade das imagens no video
     # jogafora = True
 
-    #ponto flutuante
-    #h ,w, b = frame.shape
-
+    # converter a imagen coletada em  de rgb em hsv
     hsv = (frame/255.0).astype(np.float32)
-    #print hsv[0][0][0]
-    # Convert BGR to HSV
-    #hsv = np.zeros(frame.shape)
     hsv = cv2.cvtColor(hsv, cv2.COLOR_BGR2HSV)
 
-    #print hsv[0][0][0]
+    # define o total de cores que sera analisada no momento HSV
+    lower = np.array([hue-2,0,0])
+    upper = np.array([hue2+2,1,1])
 
-    # define range of blue color in HSV
-    lower_blue = np.array([hue-2,0,0])
-    upper_blue = np.array([hue2+2,1,1])
-
-    #smothing
+    # aplicacao de smothing
     kernel = np.ones((5,5),np.float32)/25
     hsv = cv2.filter2D(hsv,-1,kernel)
+    # aplicacao de blur na imagem
     hsv = cv2.blur(hsv,(5,5))
 
-    cv2.imshow('original',frame)
-    # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    # aplicacao de um trashold na imagem capturando para remocao do fundo
+    mask = cv2.inRange(hsv, lower, upper)
 
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame,frame, mask= mask)
 
     img_rgb = res
+
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
     if cor == 0:
         template = cv2.imread('grao.png',0)
+        threshold = 0.6
     if cor == 1:
-        template = cv2.imread('graoBom.png',0)
+        template = cv2.imread('graoBomTest.png',0)
+        threshold = 0.75
     if cor == 2:
-        template = cv2.imread('graoMarron.png',0)
+        template = cv2.imread('graoMarronTest.png',0)
+        threshold = 0.6
     w, h = template.shape[::-1]
 
     rescir = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-    threshold = 0.6
+    #threshold = 0.75
     loc = np.where( rescir >= threshold)
     count = 0
     listPoit = []
+    #busca dos elementos similares
     for pt in zip(*loc[::-1]):
         if listPoit == []:
             listPoit.append(pt);
@@ -122,7 +107,10 @@ for cor in range(0,3):
                 cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
                 listPoit.append(pt)
                 count += 1
-    #cv2.imshow('circulos',img_rgb)
+    # exibicao da imagen original
+    cv2.imshow('original',frame)
+
+    # exibicao da imagen pela separacao de cor
     if cor == 0:
         cv2.imshow('Verde',res)
     if cor == 1:
@@ -131,38 +119,25 @@ for cor in range(0,3):
         cv2.imshow('Marron',res)
     print count
     contabil.append(count)
-    # cv2.imshow('frame',frame)
-    # cv2.imshow('mask',mask)
-    #cv2.imshow('res',res)
-print contabil
-# make a square figure and axes
-figure(1, figsize=(6,6))
-ax = axes([0.1, 0.1, 0.8, 0.8])
 
-# The slices will be ordered and plotted counter-clockwise.
-labels = 'verde', 'bom', 'marron','outros'
-
+#calculo dos outros graos que ainda nao foram verificados
 contar = 0
 for ct in contabil:
     contar += ct
+contabil.append(20 - contar)
 
-contabil.append(32 - contar)
+#geracao do grafico em pizza
+figure(1, figsize=(6,6))
+ax = axes([0.1, 0.1, 0.8, 0.8])
+labels = 'verde', 'bom', 'marron','outros'
 fracs = contabil
 explode=(0, 0.05, 0, 0)
-
-pie(fracs, explode=explode, labels=labels,
-                autopct='%1.1f%%', shadow=True, startangle=90)
-                # The default startangle is 0, which would start
-                # the Frogs slice on the x-axis.  With startangle=90,
-                # everything is rotated counter-clockwise by 90 degrees,
-                # so the plotting starts on the positive y-axis.
-
+pie(fracs, explode=explode, labels=labels,autopct='%1.1f%%', shadow=True, startangle=90)
 title('Analise dos graos', bbox={'facecolor':'0.8', 'pad':5})
-
 show()
+#comando para finalizar o codigo
 while True:
     ch = cv2.waitKey(27)
     if ch == 27:#Escape
         cv2.destroyAllWindows()
-
         break
